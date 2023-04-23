bf0504c291e9882eca7bce8d199d9bbabe80f0c3
Vladislav Vertiyov <v.vertiyov@yandex.ru>
2023-04-24 00:21:19 +0300

    Add user model to admin

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+class Recipe(models.Model):
+    pass
+
+class Tag(models.Model):
+    pass
+
+class Ingridient(models.Model):
+    pass
+
+class FavoriteList(models.Model):
+    pass
+
+class ShoppingCart(models.Model):
+    pass
+
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'username', 'email')
+    list_filter = ('username', 'email')
+
+admin.site.register(User, UserAdmin)
