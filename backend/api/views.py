032232f29d413fc3c930b6ae137a65f98a011fc1
Vladislav Vertiyov <v.vertiyov@yandex.ru>
2023-05-14 02:40:54 +0300

    ReDoc Tags part DONE

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,8 +4,8 @@ from rest_framework import mixins, viewsets, status, filters
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
-from recipes.models import Ingredient
-from .serializers import IngredientSerializer
+from recipes.models import Ingredient, Tag
+from .serializers import IngredientSerializer, TagSerializer
 
 class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Ingredient.objects.all()
@@ -15,3 +15,8 @@ class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
     search_fields = ('^name',)
     pagination_class = None
 
+class TagViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    permission_classes = (AllowAny,)
+    pagination_class = None
