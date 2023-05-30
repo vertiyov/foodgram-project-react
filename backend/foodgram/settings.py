2e22f7442223d55aa010776ab474859c7a19e3f9
Vladislav Vertiyov <v.vertiyov@yandex.ru>
2023-05-30 16:36:20 +0300

    Review #5

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -97,21 +97,6 @@ DATABASES = {
 }
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 AUTH_USER_MODEL = 'users.User'
 
 # Password validation
